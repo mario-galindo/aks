95ed4166b283dfb3236107eaad973c575ee548cf
mario-galindo <mgalindo9410@gmail.com>
2022-01-31 10:42:28 -0600

    concat method changed

--- cluster.tf
+++ cluster.tf
@@ -2,7 +2,7 @@ resource "azurerm_kubernetes_cluster" "myclusteraks" {
   name                = "mycluster-aks"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  node_resource_group = concat(azurerm_resource_group.rg.name,"-","ancillary")
+  node_resource_group = "${azurerm_resource_group.rg.name}-ancillary"
   dns_prefix          = "myclusteraks"
 
   default_node_pool {
